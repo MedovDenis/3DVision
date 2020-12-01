3284fcec8bd908f813a48f23293905610c2ff324
Medov <medovdenis@gmail.com>
2020-12-01 09:03:56 +0300

    Повороты и перемещение

--- 3DVision.py
+++ 3DVision.py
@@ -1,35 +1,89 @@
 from tkinter import *
 import  math
-from PIL import Image, ImageTk
+# from PIL import Image, ImageTk
 
 # class Main (Frame):
 #     def __init__(self, root):
 #         super().__init__(root)
 
-def btnBall_Click(event):
-    global figure
-    figure = Ball(100)
-    print(figure)
+af = 0.1
+gm = 0.1
+dx = 0
+dy = 0
+width = 500
+height = 500
+
+def btnUpRot_Click():
+    global af
+    af = af + 0.1
+    # print(figure)
     Print()
 
-def MouseLeft(event):
-    global lbDebug
-    lbDebug.config(text = 'Левая кнопка мыши')      
+def btnDownRot_Click():
+    global af
+    af = af - 0.1
+    # print(figure)
+    Print()
+
+def btnRightRot_Click():
+    global gm
+    gm = gm + 0.1
+    # print(figure)
+    Print()
+
+def btnLeftRot_Click():
+    global gm
+    gm = gm - 0.1
+    # print(figure)
+    Print()
+
+
+def btnUp_Click():
+    global dy
+    dy = dy - 10
+    # print(figure)
+    Print()
+
+def btnDown_Click():
+    global dy
+    dy = dy + 10
+    # print(figure)
+    Print()
+
+def btnRight_Click():
+    global dx
+    dx = dx + 10
+    # print(figure)
+    Print()
+
+def btnLeft_Click():
+    global dx
+    dx = dx - 10
+    # print(figure)
+    Print()
+
+def btnBall_Click():
+    global figure
+    figure = Ball(100)
+    # print(figure)
+    Print()
+
+# def MouseLeft(event):
+#     global lbDebug
+#     lbDebug.config(text = 'Левая кнопка мыши')      
  
-def MouseRight(event):
-    global lbDebug
-    lbDebug.config(text = 'Правая кнопка мыши')    
+# def MouseRight(event):
+#     global lbDebug
+#     lbDebug.config(text = 'Правая кнопка мыши')    
 
-def MouseMove(event):
-    global lbDebug
-    # if event.find('Button1') != -1:
-    #     lbDebug.config(text = 'Правая кнопка мыши')   
-    print(event)
-    x = event.x
-    y = event.y
-    s = "Движение мышью {}x{}".format(x, y)
+# def MouseMove(event):
+#     global lbDebug
+#     print(event)
+#     x = event.x
+#     y = event.y
+#     s = "Движение мышью {}x{}".format(x, y)
 
-    lbDebug.config(text = s)
+#     lbDebug.config(text = s)
 
 # =========== Figure ===========
 def Ball(R):
@@ -47,8 +101,8 @@ def Ball(R):
             point3D['y'] = y
             point3D['z'] = z
             listpoint.append(point3D)
-            j = j + 0.1
-        i = i + 0.1
+            j = j + 0.05
+        i = i + 0.05
             
     return listpoint
 
@@ -70,7 +124,7 @@ def Cube(Width):
 
 def Print ():
     global figure, cavans
-
+    cavans.delete("all") 
     i = -1
     for point3D in figure:
         point0 = Convert2D(figure[i])
@@ -86,10 +140,9 @@ def Print ():
         cavans.create_line(x0, y0, x1, y1)
 
 def Convert2D (point3D):
-    af = 0.1
-    gm = 0.1
-    x = point3D['x'] * math.cos(gm) - point3D['y'] * math.sin(gm) + 500 / 2
-    y = math.cos(af) * (point3D['x'] * math.sin(gm) + point3D['y'] * math.cos(gm)) + point3D['z'] * math.cos(af) + 500 / 2
+    global af, gm, width, height
+    x = point3D['x'] * math.cos(gm) - point3D['y'] * math.sin(gm) + width / 2 + dx
+    y = math.sin(af) * (point3D['x'] * math.sin(gm) + point3D['y'] * math.cos(gm)) + point3D['z'] * math.cos(af) + height / 2 + dy
 
     point = {'x' : x, 'y' : y}
     return point
@@ -98,12 +151,14 @@ if __name__ == "__main__":
     root = Tk()
     figure = []
 
+
+
     # =========== Form ===========
     root.iconbitmap('sours/3d-model.ico')
     root.title('3DVision')
 
     # =========== Button ===========
-    btnBall = Button(text = "Шар")
+    btnBall = Button(text = "Шар", command = btnBall_Click)
     btnBall.grid(row = 0, column = 0)
 
     btnCube = Button(text = "Куб")
@@ -112,20 +167,52 @@ if __name__ == "__main__":
     btnPrizm = Button(text = "Призма")
     btnPrizm.grid(row = 0, column = 2)
 
+    # =========== Moving ===========
+    btnUp = Button(text = "Вверх", command = btnUp_Click)
+    btnUp.grid(row = 3, column = 1)
+
+    btnDown = Button(text = "Вниз", command = btnDown_Click)
+    btnDown.grid(row = 5, column = 1)
+
+    btnRight = Button(text = "Вправо", command = btnRight_Click)
+    btnRight.grid(row = 4, column = 2)
+
+    btnLeft = Button(text = "Влево", command = btnLeft_Click)
+    btnLeft.grid(row = 4, column = 0) 
+
+    # =========== Rotation ===========
+    btnUpRot = Button(text = "Вверх", command = btnUpRot_Click)
+    btnUpRot.grid(row = 3, column = 4)
+
+    btnDownRot = Button(text = "Вниз", command = btnDownRot_Click)
+    btnDownRot.grid(row = 5, column = 4)
+
+    btnRightRot = Button(text = "Вправо", command = btnRightRot_Click)
+    btnRightRot.grid(row = 4, column = 5)
+
+    btnLeftRot = Button(text = "Влево", command = btnLeftRot_Click)
+    btnLeftRot.grid(row = 4, column = 3) 
+
     # =========== Canvas ===========
-    cavans = Canvas(root, width=500, height=500, bg='white')
-    cavans.grid(row = 1, column = 0, columnspan = 3)
+    cavans = Canvas(root, width = width, height = height, bg='white')
+    cavans.grid(row = 1, column = 0, columnspan = 6)
 
     # =========== Label ===========
-    lbDebug = Label(text = '')
-    lbDebug.grid(row = 2,  column = 0, columnspan = 3)                
+    # lbDebug = Label(text = '')
+    # lbDebug.grid(row = 5,  column = 0, columnspan = 3)
+
+    lbDebug = Label(text = 'Перемещение')
+    lbDebug.grid(row = 2,  column = 0, columnspan = 3)
+
+    lbDebug = Label(text = 'Поворот')
+    lbDebug.grid(row = 2,  column = 3, columnspan = 3)                     
  
     # =========== Bind ===========
-    cavans.bind('<Button-1>', MouseLeft)
-    cavans.bind('<Button-3>', MouseRight)
-    cavans.bind('<Motion>', MouseMove)
+    # cavans.bind('<Button-1>', MouseLeft)
+    # cavans.bind('<Button-3>', MouseRight)
+    # cavans.bind('<Motion>', MouseMove)
 
-    btnBall.bind('<Button-1>', btnBall_Click)
+    # btnBall.bind('<Button-1>', btnBall_Click)
     
     root.mainloop()
 
